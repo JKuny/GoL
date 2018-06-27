ef2ee66689edb41b7065df4dba44667218297159
James Kuny <james.kuny@gmail.com>
2018-06-27 13:49:44 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,20 +1,32 @@
+# 06/25/2018
+#
+# Tests for IdeaGenerator
+# By James Kuny - james.kuny@yahoo.com
+
 import unittest
 import ideas
 
 class TestIdeaGenerator(unittest.TestCase):
 
 	def setUp(self):
-		self.x = ideas.IdeaGenerator(1)
+		self.x = ideas.IdeaGenerator(3)
+		
+	def test_initialized_array(self):
+		self.assertEqual(len(self.x.results), 0)
 
 	def test_parameter(self):
-		self.assertEqual(self.x.numberOfIdeas, 1)
+		self.assertEqual(self.x.numberOfIdeas, 3)
 
 	def test_output(self):
-		self.assertEqual(self.x.generate_ideas(), "idea")
-
+		self.x.generate_ideas()
+		self.assertTrue(len(self.x.results) > 0)
+		
+	def test_file_found(self):
+		self.assertIsNotNone(self.x.is_file_found())
+		
 	def test_file_read(self):
-		self.assertEqual("", "")
-
+		self.assertIsNotNone(self.x.retrieve_noun())
+		
 if __name__ == "__main__":
 	global x
 	unittest.main()
