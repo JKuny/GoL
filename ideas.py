95f438c8b6b3568349d165ed8dece7d141f6d80d
James Kuny <james.kuny@yahoo.com>
2019-02-10 16:52:05 -0500

    Changed methods to static ones

--- ideas.py
+++ ideas.py
@@ -30,19 +30,21 @@ class IdeaGenerator(object):
     def generate_sentence(self):
         """ Creates a basic sentence with random nouns and adjectives. """
         sentence = "An image of a "
-        sentence = sentence + self.retrieve_file_info("nouns")
+        sentence = sentence + IdeaGenerator.retrieve_file_info("nouns")
         sentence = sentence + " that is "
-        sentence = sentence + self.retrieve_file_info("adjectives")
+        sentence = sentence + IdeaGenerator.retrieve_file_info("adjectives")
         sentence = sentence + "."
         return sentence
 
-    def is_file_found(self, file_name):
+    @staticmethod
+    def is_file_found(file_name):
         """ Checks to see if the specified file exists. """
         file = open(file_name + ".txt", 'r')
         file.close()
         return file
 
-    def retrieve_file_info(self, file_name):
+    @staticmethod
+    def retrieve_file_info(file_name):
         """ Return a random value from the specified file. """
         file = open(file_name + ".txt", 'r')
 
