867a5671f8d2b69e76608b98fcb833d37ab2e064
James Kuny <james.kuny@yahoo.com>
2019-02-10 16:38:19 -0500

    Reformatted docstrings to proper form

--- ideas.py
+++ ideas.py
@@ -7,60 +7,56 @@ import sys
 import os
 import random
 
+
 class IdeaGenerator(object):
-	
-	"""
-	Main constructor.
-	"""
-	def __init__(self, number_of_ideas):
-		os.chdir(r'.') # Change directory to where the file is
-		self.results = []
-		self.number_of_ideas = number_of_ideas
 
-	"""
-	Creates populate's the generator's result array with
-	the object's specied number of ideas.
-	"""
-	def generate_ideas(self):
-		ideaArray = []
-		for x in range(0, self.number_of_ideas):
-			ideaArray.append(self.generate_sentence())
-			
-		self.results = ideaArray
+    def __init__(self, number_of_ideas):
+        """ Main constructor. """
+        os.chdir(r'.')  # Change directory to where the file is
+        self.results = []
+        self.number_of_ideas = number_of_ideas
 
-	"""
-	Creates a basic sentence with random nouns and adjectives.
-	"""
-	def generate_sentence(self):
-		sentence = "An image of a "
-		sentence = sentence + self.retrieve_file_info("nouns")
-		sentence = sentence + " that is "
-		sentence = sentence + self.retrieve_file_info("adjectives")
-		sentence = sentence + "."
-		return sentence
+    def generate_ideas(self):
+        """
+        Creates populate's the generator's result array with
+        the object's specied number of ideas.
+        """
+        ideaArray = []
+        for x in range(0, self.number_of_ideas):
+            ideaArray.append(self.generate_sentence())
 
-	"""
-	Checks to see if the specified file exists.
-	"""
-	def is_file_found(self, file_name):
-		file = open(file_name + ".txt", 'r')
-		file.close()
-		return file
-		
-	""" Return a random value from the specified file. """
-	def retrieve_file_info(self, file_name):
-		file = open(file_name + ".txt", 'r')
+        self.results = ideaArray
 
-		array = []
-		for line in file:
-			array.append(line.rstrip())
+    def generate_sentence(self):
+        """ Creates a basic sentence with random nouns and adjectives. """
+        sentence = "An image of a "
+        sentence = sentence + self.retrieve_file_info("nouns")
+        sentence = sentence + " that is "
+        sentence = sentence + self.retrieve_file_info("adjectives")
+        sentence = sentence + "."
+        return sentence
 
-		file.close()
+    def is_file_found(self, file_name):
+        """ Checks to see if the specified file exists. """
+        file = open(file_name + ".txt", 'r')
+        file.close()
+        return file
 
-		index = random.randint(0, len(array) - 1)
-		return array[index]
+    def retrieve_file_info(self, file_name):
+        """ Return a random value from the specified file. """
+        file = open(file_name + ".txt", 'r')
+
+        array = []
+        for line in file:
+            array.append(line.rstrip())
+
+        file.close()
+
+        index = random.randint(0, len(array) - 1)
+        return array[index]
+
 
 if __name__ == '__main__':
-	x = IdeaGenerator(1)
-	x.generate_ideas()
-	print(x.results[0])
+    x = IdeaGenerator(1)
+    x.generate_ideas()
+    print(x.results[0])
--- tests.py
+++ tests.py
@@ -6,32 +6,35 @@
 import unittest
 import ideas
 
+
 class TestIdeaGenerator(unittest.TestCase):
 
-	def setUp(self):
-		self.test_generator = ideas.IdeaGenerator(3)
-		
-	def test_initialized_array(self):
-		self.assertEqual(len(self.test_generator.results), 0)
+    def setUp(self):
+        self.test_generator = ideas.IdeaGenerator(3)
 
-	def test_parameter(self):
-		self.assertEqual(self.test_generator.number_of_ideas, 3)
+    def test_initialized_array(self):
+        self.assertEqual(len(self.test_generator.results), 0)
 
-	def test_output(self):
-		self.test_generator.generate_ideas()
-		self.assertTrue(len(self.test_generator.results) > 0)
-		
-	def test_file_found(self):
-		self.assertIsNotNone(self.test_generator.is_file_found("nouns"))
-		self.assertIsNotNone(self.test_generator.is_file_found("adjectives"))
-	
-	def test_file_read(self):
-		self.assertIsNotNone(self.test_generator.retrieve_file_info("nouns"))
-		self.assertIsNotNone(self.test_generator.retrieve_file_info("adjectives"))
-		
-	def test_sentence_made(self):
-		self.assertIsNotNone(self.test_generator.generate_sentence())
-		
+    def test_parameter(self):
+        self.assertEqual(self.test_generator.number_of_ideas, 3)
+
+    def test_output(self):
+        self.test_generator.generate_ideas()
+        self.assertTrue(len(self.test_generator.results) > 0)
+
+    def test_file_found(self):
+        self.assertIsNotNone(self.test_generator.is_file_found("nouns"))
+        self.assertIsNotNone(self.test_generator.is_file_found("adjectives"))
+
+    def test_file_read(self):
+        self.assertIsNotNone(self.test_generator.retrieve_file_info("nouns"))
+        self.assertIsNotNone(
+            self.test_generator.retrieve_file_info("adjectives"))
+
+    def test_sentence_made(self):
+        self.assertIsNotNone(self.test_generator.generate_sentence())
+
+
 if __name__ == "__main__":
-	global x
-	unittest.main()
+    global x
+    unittest.main()
